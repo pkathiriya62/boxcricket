bb304538e4c4d29ae86b869a6fe784807d74fe27
Prince kathiriya <princekathiriya62@gmail.com>
2023-12-02 18:20:57 +0530

    boxcricket

--- lib/main.dart
+++ lib/main.dart
@@ -18,7 +18,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Screen2(),
+      home: const Screen2(),  
     );
   }
 }
