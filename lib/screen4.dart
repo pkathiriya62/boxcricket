bd883b4f1c9876499a710b48132fd64f87abb3d6
Prince kathiriya <princekathiriya62@gmail.com>
2023-12-14 18:49:36 +0530

    screen4

--- lib/screen4.dart
+++ lib/screen4.dart
@@ -8,13 +8,13 @@ class Screen4 extends StatelessWidget {
     return Scaffold(
       body: Column(
         children: [
-          SizedBox(
+          const SizedBox(
             height: 50,
           ),
           Container(
             height: 60,
             width: double.infinity,
-            decoration: BoxDecoration(
+            decoration: const BoxDecoration(
               color: Color(0xff78A408),
             ),
             child: Center(
@@ -25,27 +25,30 @@ class Screen4 extends StatelessWidget {
                     child: Container(
                       height: 40,
                       width: 40,
-                      decoration: BoxDecoration(
-                          color: Colors.white,
-                          shape: BoxShape.circle,
-                          image: DecorationImage(
-                              image: NetworkImage(
-                                  'https://media.licdn.com/dms/image/C4D03AQHHSwZyheu1UQ/profile-displayphoto-shrink_800_800/0/1662703260529?e=2147483647&v=beta&t=Jm4eBGPY71_8duu5EDtQrXPoUY9yMY-QcRh5OiTztEk'))),
+                      decoration: const BoxDecoration(
+                        color: Colors.white,
+                        shape: BoxShape.circle,
+                        image: DecorationImage(
+                          image: AssetImage(
+                            'assets/images/Mask group.png',
+                          ),
+                        ),
+                      ),
                     ),
                   ),
-                  Padding(
-                    padding: const EdgeInsets.all(8.0),
+                  const Padding(
+                    padding: EdgeInsets.all(8.0),
                     child: Text(
-                      'Hello,Modiji!',
+                      'Hello,Vaibhav!',
                       style:
                           TextStyle(fontSize: 20, fontWeight: FontWeight.w700),
                     ),
                   ),
-                  SizedBox(
-                    width: 150,
+                  const SizedBox(
+                    width: 130,
                   ),
-                  Padding(
-                    padding: const EdgeInsets.only(right: 20),
+                  const Padding(
+                    padding: EdgeInsets.only(right: 20),
                     child: Icon(
                       Icons.notifications_none,
                       size: 30,
@@ -55,20 +58,24 @@ class Screen4 extends StatelessWidget {
               ),
             ),
           ),
-          SizedBox(
+          const SizedBox(
             height: 20,
           ),
           Padding(
             padding: const EdgeInsets.all(9.0),
             child: TextFormField(
-              
               decoration: InputDecoration(
                 border: OutlineInputBorder(
                   borderRadius: BorderRadius.circular(12),
                 ),
-                
-                suffixIcon: Icon(Icons.mic,size: 30,),
-                prefixIcon: Icon(Icons.search,size: 30,),
+                suffixIcon: const Icon(
+                  Icons.mic,
+                  size: 30,
+                ),
+                prefixIcon: const Icon(
+                  Icons.search,
+                  size: 30,
+                ),
                 hintText: ('Search'),
               ),
             ),
