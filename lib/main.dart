44e0d3244f99941bbe790ac3e9f3d13e56e3d4b5
Prince kathiriya <princekathiriya62@gmail.com>
2023-12-20 18:08:37 +0530

    box

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 // import 'package:boxcricket2/conformation.dart';
+import 'package:boxcricket2/details.dart';
 import 'package:boxcricket2/filter.dart';
 import 'package:flutter/material.dart';
 
@@ -19,7 +20,7 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-      home: const Screen10(),  
+      home: const DetailsScreen(),  
     );
   }
 }
